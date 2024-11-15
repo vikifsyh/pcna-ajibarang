2a42c2d7f18cb39fa16a12082b539de7068176d0
Viki Flendiansyah <vfsyah@gmail.com>
2024-11-16 00:57:07 +0700

    feat: article

--- src/app/berita/[id]/page.tsx
+++ src/app/berita/[id]/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useParams } from "next/navigation"; // Mengambil ID dari URL
+import { useParams, useRouter } from "next/navigation";
 import { formatDistanceToNow } from "date-fns";
+import Link from "next/link";
+import { ArrowLeftIcon } from "@heroicons/react/16/solid";
+import Image from "next/image";
+import { ArrowRightIcon } from "@heroicons/react/16/solid";
 
 interface Article {
   id: string;
@@ -14,10 +18,12 @@ interface Article {
 
 export default function ArticleDetail() {
   const [article, setArticle] = useState<Article | null>(null);
+  const [recommendations, setRecommendations] = useState<Article[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
   const { id } = useParams(); // Mendapatkan id dari URL
+  const router = useRouter();
 
   // Fetch single article by ID
   const fetchArticle = async (id: string) => {
@@ -29,6 +35,14 @@ export default function ArticleDetail() {
       }
       const data = await res.json();
       setArticle(data.article);
+
+      // Fetch recommendations (articles excluding the current one)
+      const recommendationsRes = await fetch(`/api/article?exclude=${id}`);
+      if (!recommendationsRes.ok) {
+        throw new Error("Failed to fetch recommendations.");
+      }
+      const recommendationsData = await recommendationsRes.json();
+      setRecommendations(recommendationsData.articles);
     } catch (err: any) {
       setError(err.message || "Something went wrong.");
     } finally {
@@ -50,37 +64,89 @@ export default function ArticleDetail() {
 
   // Fungsi untuk memformat konten artikel menjadi paragraf
   const formatContentToParagraphs = (content: string) => {
-    // Misalnya, jika konten dipisahkan dengan \n atau <br> dan kita ingin membagi menjadi paragraf
     return content.split("\n").map((para, index) => (
-      <p key={index} className="mb-4">
+      <p key={index} className="mb-4 text-justify">
         {para}
       </p>
     ));
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <button
-        className="mb-4 text-blue-500"
-        onClick={() => window.history.back()}
-      >
-        Back to Articles
-      </button>
-      <div className="border p-4 rounded shadow">
-        <img
-          src={article.image}
-          alt={article.title}
-          className="w-full h-64 object-cover rounded mb-4"
-        />
-        <h2 className="text-2xl font-bold mb-2">{article.title}</h2>
-        <p className="text-sm text-gray-600 mb-4">
-          {formatDistanceToNow(new Date(article.createdAt), {
-            addSuffix: true,
-          })}
-        </p>
+    <div className="mx-5 md:mx-20">
+      {/* Kolom Utama (Detail Artikel) */}
+      <div className="">
+        <Link
+          className="mb-4 inline-flex items-center text-base font-medium text-neutral-400"
+          href={"/berita"}
+        >
+          <ArrowLeftIcon className="mr-2 h-4 w-4" />
+          Kembali
+        </Link>
+        <h5 className="text-base md:text-lg text-neutral-400">
+          Baca berita terkini seputar PCNA Ajibarang
+        </h5>
 
-        {/* Menampilkan konten artikel dalam bentuk paragraf */}
-        {formatContentToParagraphs(article.content)}
+        <div className="md:flex md:justify-between gap-10">
+          <div className="mt-4 max-w-3xl">
+            <h2 className="text-2xl font-bold mb-2">{article.title}</h2>
+            <p className="text-sm text-primary mb-4">
+              {formatDistanceToNow(new Date(article.createdAt), {
+                addSuffix: true,
+              })}
+            </p>
+            <Image
+              src={article.image}
+              alt={article.title}
+              width={1000}
+              height={1000}
+              className="w-full h-auto object-cover rounded mb-4"
+            />
+
+            {formatContentToParagraphs(article.content)}
+          </div>
+
+          {/* Kolom Kanan (Artikel Rekomendasi) */}
+          <div className="mt-10 md:mt-4 max-w-lg">
+            <h3 className="text-lg font-bold mb-4">Baca berita lainnya</h3>
+            <div className="">
+              {recommendations.map((recommendation) => (
+                <div
+                  key={recommendation.id}
+                  className="flex items-start space-x-4"
+                >
+                  <Image
+                    src={recommendation.image}
+                    alt={recommendation.title}
+                    className="w-[160px] h-[160px] object-cover rounded"
+                    width={1000}
+                    height={1000}
+                  />
+                  <div className="space-y-4 md:space-y-8">
+                    <div>
+                      <Link
+                        href={`/berita/${recommendation.id}`}
+                        className="font-medium text-blue-600 hover:underline"
+                      >
+                        {recommendation.title}
+                      </Link>
+                      <p className="text-sm text-gray-600 mt-2">
+                        {formatDistanceToNow(
+                          new Date(recommendation.createdAt),
+                          {
+                            addSuffix: true,
+                          }
+                        )}
+                      </p>
+                    </div>
+                    <button className="inline-flex items-center text-base font-medium px-3 py-2 border rounded-md">
+                      Baca berita <ArrowRightIcon className="ml-2 h-4 w-4" />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/app/berita/page.tsx
+++ src/app/berita/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 import { formatDistanceToNow } from "date-fns";
 import Image from "next/image";
 import Link from "next/link";
+import { ArrowRightIcon } from "@heroicons/react/16/solid";
 
 interface Article {
   id: string;
@@ -18,8 +18,6 @@ export default function Articles() {
   const [articles, setArticles] = useState<Article[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
-
-  const router = useRouter();
 
   // Fetch all articles
   const fetchArticles = async () => {
@@ -42,35 +40,73 @@ export default function Articles() {
     fetchArticles();
   }, []);
 
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Articles</h1>
+  const SkeletonCard = () => (
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg animate-pulse">
+      <div className="w-full h-48 bg-gray-200 rounded-t-lg"></div>
+      <div className="p-5">
+        <div className="h-4 bg-gray-200 rounded w-1/3 mb-4"></div>
+        <div className="h-6 bg-gray-200 rounded w-full mb-2"></div>
+        <div className="h-6 bg-gray-200 rounded w-5/6 mb-4"></div>
+        <div className="h-8 bg-blue-200 rounded w-1/2"></div>
+      </div>
+    </div>
+  );
 
-      {loading && <p>Loading...</p>}
+  return (
+    <div className="mx-5 md:mx-20">
+      <div className="text-center">
+        <h1 className="text-3xl md:text-4xl font-bold mb-2 md:mb-4">
+          PCNA Ajibarang News
+        </h1>
+
+        <p className="text-gray-600 mb-2 md:mb-4">
+          Berita terkini tentang PCNA Ajibarang.
+        </p>
+      </div>
+
       {error && <p className="text-red-500">{error}</p>}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {articles.map((article) => (
-          <Link
-            key={article.id}
-            className="border p-4 rounded shadow hover:shadow-lg cursor-pointer"
-            href={`/berita/${article.id}`}
-          >
-            <Image
-              src={article.image}
-              alt={article.title}
-              className="w-full h-48 object-cover rounded mb-2"
-              width={1000}
-              height={1000}
-            />
-            <h2 className="text-lg font-semibold">{article.title}</h2>
-            <p className="text-sm text-gray-600">
-              {formatDistanceToNow(new Date(article.createdAt), {
-                addSuffix: true,
-              })}
-            </p>
-          </Link>
-        ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 md:mt-20">
+        {loading
+          ? Array.from({ length: 6 }).map((_, index) => (
+              <SkeletonCard key={index} />
+            ))
+          : articles.map((article) => (
+              <div
+                key={article.id}
+                className="max-w-sm bg-white border border-gray-200 rounded-lg"
+              >
+                <div className="w-full">
+                  <Image
+                    className="rounded-t-lg object-cover"
+                    width={1000}
+                    height={1000}
+                    src={article.image}
+                    alt={article.title}
+                  />
+                </div>
+                <div className="p-5">
+                  <p className="text-sm text-gray-600">
+                    {formatDistanceToNow(new Date(article.createdAt), {
+                      addSuffix: true,
+                    })}
+                  </p>
+                  <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 mt-4">
+                    {article.title}
+                  </h5>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 line-clamp-2">
+                    {article.content}
+                  </p>
+                  <Link
+                    href={`/berita/${article.id}`}
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 mt-10"
+                  >
+                    Baca Selengkapnya
+                    <ArrowRightIcon className="w-4 h-4 ml-2" />
+                  </Link>
+                </div>
+              </div>
+            ))}
       </div>
     </div>
   );
